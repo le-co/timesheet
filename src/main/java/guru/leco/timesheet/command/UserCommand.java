f5a2a08e6a440183eb6bb6eb78b24ca20f8bca44
leco <lcosta.sp.br@gmail.com>
2018-12-04 09:42:16 -0200

    Create user command. closes #28

---
+++ src/main/java/guru/leco/timesheet/command/UserCommand.java
@@ -0,0 +1,23 @@
+package guru.leco.timesheet.command;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class UserCommand {
+
+    private String id;
+
+    private String mail;
+
+    private String code;
+
+    private String password;
+
+    private String username;
+
+    private List<PointingCommand> pointing;
+}
